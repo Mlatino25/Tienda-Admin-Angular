95e6a5b823420c1844e98b4dab0748951818bf4c
Miguel Latino <latino2000miguel@gmail.com>
2024-08-31 19:22:49 -0500

    commit 3

--- karma.conf.js
+++ karma.conf.js
@@ -2,9 +2,16 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  const isCI = process.env.CI === 'true'; // Detecta si está en un entorno de CI/CD
+
   config.set({
+    // Base path for resolving files and excluding patterns
     basePath: '',
+
+    // Frameworks to use
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    // List of plugins to load
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -12,6 +19,33 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
+    // List of browsers to launch
+    // En CI/CD, usa ChromeHeadless; en desarrollo, usa Chrome
+    browsers: isCI ? ['ChromeHeadless'] : ['Chrome'],
+
+    // Additional configuration for ChromeHeadless
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+
+    // Reporting options
+    reporters: ['progress', 'kjhtml'],
+
+    // Configuration for code coverage reporting
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/mantis-free-version'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+
+    // Configure client options
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
@@ -21,21 +55,18 @@ module.exports = function (config) {
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
+    // Misc settings
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mantis-free-version'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }]
-    },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+
+    // Configuración automática según el entorno
+    autoWatch: !isCI, // Habilita autoWatch en desarrollo, desactívalo en CI/CD
+    singleRun: isCI,  // Ejecuta las pruebas una sola vez en CI/CD, mantén Karma en ejecución en desarrollo
+    restartOnFileChange: !isCI // Habilita el reinicio automático de pruebas en desarrollo, desactívalo en CI/CD
   });
 };
