8c59d78375ab293097872451dde9774b70d251a7
Miguel Latino <latino2000miguel@gmail.com>
2024-08-31 19:55:01 -0500

    commit o7

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,17 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateChildFn } from '@angular/router';
-
+import { Router } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { AuthService } from '../auth.service';
+import { of } from 'rxjs';
 
 describe('AuthGuard', () => {
-  const executeGuard: CanActivateChildFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => AuthGuard(...guardParameters));
+  let guard: AuthGuard;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
+
+    guard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
+  });
+
+  it('should allow access if the user is logged in', () => {
+    (authService.isLoggedIn as jasmine.Spy).and.returnValue(true);
+
+    expect(guard.canActivate()).toBeTrue();
+  });
+
+  it('should redirect to login if the user is not logged in', () => {
+    (authService.isLoggedIn as jasmine.Spy).and.returnValue(false);
+
+    expect(guard.canActivate()).toBeFalse();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
